a5161570b3fba2f634eb15dd3e30846c78af8e9d
Kelvin <kelvinices@gmail.com>
2019-11-20 08:24:56 +0700

    adding faker

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from ui.main_window import MainWindow
 from ui.report_window import ReportWindow
-
+from faker import Faker
 
 ############################################
 def init():
     app = QApplication(sys.argv)
     width = app.desktop().screenGeometry().width()
     height = app.desktop().screenGeometry().height()
+    # fake = Faker()
+    # print(fake.address())
     w = MainWindow(width, height, "Main Window")
     sys.exit(app.exec_())
 
--- util/util.py
+++ util/util.py
@@ -1,15 +1,14 @@
 import meta
 import uuid
+from faker import Faker
 
 
 def uuid():
     return uuid.uuid4()
 
-
 class Util(metaclass=meta.Singleton):
 
     def fake(self, type):
+
         pass
 
-
-print(uuid())
