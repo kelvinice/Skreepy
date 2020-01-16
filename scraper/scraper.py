1fb7fb703121699238832a6c218a35a1e997b269
Kelvin <kelvinices@gmail.com>
2020-01-16 18:53:54 +0700

    rollback return value

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -56,8 +56,8 @@ def innerHTML(element):
     return element.encode_contents()
 
 
-def getheader(htmldata) -> dict:
-    header = dict
+def getheader(htmldata):
+    header = {}
     header["id"] = "None"
     header["class"] = "None"
 
